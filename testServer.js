bb2d40206a5dade6a2b0e34208e53a8cd38e78ab
Unknown <chikaka36@gmail.com>
2018-10-26 00:28:19 +0300

    demo websocket start

--- testServer.js
+++ testServer.js
@@ -1,11 +1,26 @@
 
-// dependencies
 const express = require('express');
-const path = require('path');
 
 const server = express();
+const path = require('path');
+const WebSocketServer = require('ws').Server;
+
+const wss = new WebSocketServer({ port: 3010 });
+wss.on('connection', (ws) => {
+  ws.on('message', (message) => {
+    console.log('received: %s', message);
+  });
+  setInterval(
+    () => ws.send(`${new Date()}`),
+    4000,
+  );
+});
+
+
 server.use(express.static(`${__dirname}/`));
-// app.use(express.static(__dirname + '/css'));   ///********
+
+// app.use(express.static(__dirname + '/css'));   //
+
 
 server.get('/', (req, res) => {
   // if(login){
@@ -19,11 +34,14 @@ server.get('/', (req, res) => {
 // server.post('/getTemp', function(req, res){
 // do somethig here
 // });
+
 server.listen(8080);
+
 
 // https://expressjs.com/en/4x/api.html#res
 // https://codeforgeek.com/2015/01/render-html-file-expressjs
 // https://hackernoon.com/nodejs-web-socket-example-tutorial-send-message-connect-express-set-up-easy-step-30347a2c5535
-// http://codular.com/node-web-sockets
+
+
 // process manager
 // http://pm2.keymetrics.io/
